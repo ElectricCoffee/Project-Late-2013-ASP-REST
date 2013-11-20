869a2e10aaec6fa08fcc6cf07588dc3c6ae746a0
TrineThune <thune89@gmail.com>
2013-11-20 16:42:27 +0100

    Login Begyndt

---
+++ REST Service/REST Service/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST_Service.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public KeyValuePair<string, string> Get([FromUri] string username, [FromUri] string password)
+        {
+            var user = new Models.User(username, password);
+            return new KeyValuePair<string, string>("token-key", user.TokenKey);
+        }
+    }
+}
---
+++ REST Service/REST Service/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_Service.Models
+{
+    public class User
+    {
+        public User(string username, string password)
+        {
+            var guid = Guid.NewGuid();
+            TokenKey = Convert.ToBase64String(guid.ToByteArray());
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string TokenKey { get; private set; }
+    }
+}
