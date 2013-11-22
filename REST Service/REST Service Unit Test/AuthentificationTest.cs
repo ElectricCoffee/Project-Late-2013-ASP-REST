d5d3c96fe952467e14081e50375b5b428933a59a
Jacob Jacobsen <jacobg.jacobsen+github@gmail.com>
2013-11-22 15:01:39 +0100

    Still trying to hunt down the unit testing bugs
    + Added App.config for the unit test project
    * Removed the TestConnection from Web.config
    * Added xml comments for most test methods
    * Circumvented Linq |DataDirectory| bug
    - removed class TestInit, it lives on as DataTest

--- REST Service/REST Service Unit Test/AuthentificationTest.cs
+++ REST Service/REST Service Unit Test/AuthentificationTest.cs
@@ -3,19 +3,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Transactions;
 
 using Rest = REST_Service;
+using System.Collections.Generic;
 
 namespace REST_Service_Unit_Test
 {
+    /// <summary>
+    /// Defines a set of tests for the authentification
+    /// </summary>
     [TestClass]
-    public class AuthentificationTest
+    public class AuthentificationTest : DataTest
     {
-        private TestInit _init;
-
+        /// <summary>
+        /// Inserts a known user into the test database
+        /// </summary>
         [TestInitialize]
-        public void Init()
+        public override void Initialize()
         {
-            _init = new TestInit();
-            _init.Context.Users.Attach(
+            base.Initialize();
+
+            _context.Users.Attach(
                 new Rest.User
                 {
                     Username = "johndoe",
@@ -24,19 +30,42 @@ namespace REST_Service_Unit_Test
             );
         }
 
+        /// <summary>
+        /// Tests a succesful authentification
+        /// </summary>
         [TestMethod, Description("An accesstoken should be received if succesful")]
         public void Authentification_Succesful()
         {
-            var expected = "";
+            var expectedKey = "AccessToken";
+
             var loginCtrl = new Rest.Controllers.LoginController();
             var actual = loginCtrl.Get("johndoe", "12345678");
 
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(Dictionary<string, string>));
+            Assert.IsNotNull(actual.Key);
+            Assert.AreEqual(expectedKey, actual.Key);
+            Assert.IsNotNull(actual.Value);
         }
 
+        /// <summary>
+        /// Tests an unsuccesful authentification
+        /// </summary>
         [TestMethod]
         public void Authentification_Unsuccesful()
         {
-            
+            var expectedKey = "error";
+            var expectedValue = "";
+
+            var loginCtrl = new Rest.Controllers.LoginController();
+            var actual = loginCtrl.Get("johndoe", "12345678");
+
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(Dictionary<string, string>));
+            Assert.IsNotNull(actual.Key);
+            Assert.AreEqual(expectedKey, actual.Key);
+            Assert.IsNotNull(actual.Value);
+            Assert.AreEqual(expectedValue, actual.Value);
         }
     }
 }
---
+++ REST Service/REST Service Unit Test/DataTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+using REST_Service;
+using System.Diagnostics;
+
+namespace REST_Service_Unit_Test
+{
+    /// <summary>
+    /// Defines a basic template for database dependent tests
+    /// </summary>
+    [TestClass]
+    public class DataTest
+    {
+        protected TransactionScope _transactionScope;
+        protected string _connectionString;
+        protected BookingSystemDataContext _context;
+
+        /// <summary>
+        /// Prepares a test database
+        /// </summary>
+        [TestInitialize]
+        public virtual void Initialize() {
+            _connectionString = ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString;
+
+            // Circumventing Linq |DataDirectory| bug
+            if (_connectionString.Contains("|DataDirectory|"))
+            {
+                var dataDirectory = AppDomain.CurrentDomain.GetData("APPBASE").ToString();
+                _connectionString = _connectionString.Replace("|DataDirectory|", dataDirectory);
+            }
+
+            EnsureDataExists();
+        }
+
+        /// <summary>
+        /// Ensures a test database with known scheme is present
+        /// and instantiates a new TransactionScope to contain further data operations
+        /// </summary>
+        public void EnsureDataExists()
+        {
+            _context = new BookingSystemDataContext(_connectionString);
+
+            // attempting to hunt down the error source
+            Debug.WriteLine(_context.Mapping.DatabaseName);
+            foreach (var invalidChar in System.IO.Path.GetInvalidPathChars())
+                Debug.Write(invalidChar);
+            foreach (var invalidChar in System.IO.Path.GetInvalidFileNameChars())
+                Debug.Write(invalidChar);
+
+            if (!_context.DatabaseExists())
+                _context.CreateDatabase();
+
+            _transactionScope = new TransactionScope(TransactionScopeOption.RequiresNew);
+        }
+
+        /// <summary>
+        /// Disposes the TransactionScope
+        /// </summary>
+        [TestCleanup]
+        public virtual void Cleanup()
+        {
+            _transactionScope.Dispose();
+        }
+    }
+}
